2eb988f32ce9e590fa9c4dfcddf780f27cc5f017
Kayla Bicol <kmbicol@math.uh.edu>
2017-09-20 16:21:15 -0500

    fixed deconvolution order for filtering

--- 2D_adr/adr_steady_mod.py
+++ 2D_adr/adr_steady_mod.py
@@ -47,8 +47,6 @@ ny = nx
 mesh = UnitSquareMesh(nx,ny) # divides [0,1]x[0,1] into 20x20 
 h = CellSize(mesh)
 
-
-
 # Define function spaces
 Q = FunctionSpace(mesh, "CG", P)
 
@@ -70,6 +68,7 @@ u_  = Function(Q)
 u_tilde = Function(Q)
 u_bar = Function(Q)
 
+#velocity = Expression(('x[0]', 'x[1]'), degree=1)
 
 ## Define expressions used in variational forms
 
@@ -156,213 +155,216 @@ g=open("P"+str(P)+"_2norm.txt","a+")
 h.write("L inf-norm & No Filter & $N=0$ & $N=1$ & $N=2$ & $N=3$ \\\\ \n \\hline \n")
 g.write("L 2-norm & No Filter & $N=0$ & $N=1$ & $N=2$ & $N=3$ \\\\ \n \\hline \n")
 
-scalename = 2
-folder ="P"+str(P)+"h1_"+str(nx)+"_delta"+str(scalename)+"h"
 
-before = "/N"+str(N)
-after = "_h1_"+str(nx)+"_delta"+str(scalename)+"h_"
+for N in range(3):
 
-out_file_u = File(folder+"/u_nofilter"+"h_"+str(nx)+".pvd")
-out_file_usupg = File(folder+"/u_SUPG_"+"h_"+str(nx)+".pvd")
-out_file_ugls = File(folder+"/u_GLS_"+"h_"+str(nx)+".pvd")
-out_file_udw = File(folder+"/u_DW_"+"h_"+str(nx)+".pvd")
+	scalename = 2
+	folder ="P"+str(P)+"h1_"+str(nx)+"_delta"+str(scalename)+"h"
 
-out_file_utilde = File(folder+"/u_tilde.pvd")
-out_file_ind1 = File(folder+before+after+"a0.pvd")
-out_file_ind2 = File(folder+before+after+"a1.pvd")
-out_file_ind3 = File(folder+before+after+"a2.pvd")
-out_file_ind4 = File(folder+before+after+"a3.pvd")
+	before = "/N"+str(N)
+	after = "_h1_"+str(nx)+"_delta"+str(scalename)+"h_"
 
-string0 = 'N = '+str(N)+' and h = 1/'+str(nx)
+	out_file_u = File(folder+"/u_nofilter"+"h_"+str(nx)+".pvd")
+	out_file_usupg = File(folder+"/u_SUPG_"+"h_"+str(nx)+".pvd")
+	out_file_ugls = File(folder+"/u_GLS_"+"h_"+str(nx)+".pvd")
+	out_file_udw = File(folder+"/u_DW_"+"h_"+str(nx)+".pvd")
 
-# Save the solution to file
+	out_file_utilde = File(folder+"/u_tilde.pvd")
+	out_file_ind1 = File(folder+before+after+"a0.pvd")
+	out_file_ind2 = File(folder+before+after+"a1.pvd")
+	out_file_ind3 = File(folder+before+after+"a2.pvd")
+	out_file_ind4 = File(folder+before+after+"a3.pvd")
 
-out_file_u << u
+	string0 = 'N = '+str(N)+' and h = 1/'+str(nx)
 
-out_file_usupg << u_SUPG
+	# Save the solution to file
 
-out_file_ugls << u_GLS
+	out_file_u << u
 
-out_file_udw << u_DW
+	out_file_usupg << u_SUPG
 
-##################################################################################
-N = 0
-# Helmholtz filter to compute the indicator function
+	out_file_ugls << u_GLS
 
-u_1tilde = TrialFunction(Q)
-u_2tilde = TrialFunction(Q)
-u_3tilde = TrialFunction(Q)
-u_4tilde = TrialFunction(Q)
-#deltaH = h*vnorm/(2*mu)
+	out_file_udw << u_DW
 
-out_file_u1tilde = File(folder+"/u1_tilde.pvd")
-out_file_u2tilde = File(folder+"/u2_tilde.pvd")
-out_file_u3tilde = File(folder+"/u3_tilde.pvd")
-out_file_u4tilde = File(folder+"/u4_tilde.pvd")
+	##################################################################################
 
+	# Helmholtz filter to compute the indicator function
 
+	u_1tilde = TrialFunction(Q)
+	u_2tilde = TrialFunction(Q)
+	u_3tilde = TrialFunction(Q)
+	u_4tilde = TrialFunction(Q)
+	#deltaH = h*vnorm/(2*mu)
 
-
-## ______________________________________________________________________ N=0
-
-
-
-def a(u_, DF):
-	# Compute the indicator function
-	indicator = Expression('sqrt((a-b)*(a-b))', degree = 2, a = u_, b = DF)
-	indicator = interpolate(indicator, Q)
-	max_ind = np.amax(indicator.vector().array())
-
-	# Normalize indicator such that it's between [0,1].
-	if max_ind < 1:
-	   max_ind = 1.0
-
-	indicator = Expression('a/b', degree = 2, a = indicator, b = max_ind)
-	indicator = interpolate(indicator, Q)
-	return indicator
-
-
-
-F_Hfilter0 = v*u_1tilde*dx + delta*delta*dot(grad(v), grad(u_1tilde))*dx - v*u*dx
-
-a_Hfilter0 = lhs(F_Hfilter0)
-L_Hfilter0 = rhs(F_Hfilter0)
-
-A_Hfilter0 = assemble(a_Hfilter0)
-bc.apply(A_Hfilter0)
-
-b_Hfilter0 = assemble(L_Hfilter0)
-bc.apply(b_Hfilter0)
-
-solver0 = LUSolver(A_Hfilter0)
-u_1tilde = Function(Q)
-solver0.solve(u_1tilde.vector(), b_Hfilter0)
-DF = u_1tilde
-out_file_u1tilde << u_1tilde
-
-ind1 = a(u_tilde, DF)
-out_file_ind1 << ind1
-if N == 0:
-	ind = ind1
-
-## ______________________________________________________________________ N=1
-if N>0:
-	F_Hfilter1 = v*u_2tilde*dx + delta*delta*dot(grad(v), grad(u_2tilde))*dx - v*u_1tilde*dx
-
-	a_Hfilter1 = lhs(F_Hfilter1)
-	L_Hfilter1 = rhs(F_Hfilter1)
-
-	A_Hfilter1 = assemble(a_Hfilter1)
-	bc.apply(A_Hfilter1)
-
-	b_Hfilter1 = assemble(L_Hfilter1)
-	bc.apply(b_Hfilter1)
-
-	solver1 = LUSolver(A_Hfilter1)
-	u_2tilde = Function(Q)
-	solver1.solve(u_2tilde.vector(), b_Hfilter1)
-	DF = Expression('a+b-c',degree=2,a=DF,b=u_1tilde,c=u_2tilde)
-	out_file_u2tilde << u_2tilde
-	ind2 = a(u_tilde, DF)
-	out_file_ind2 << ind2
-	if N==1:
-		ind = ind2
-
-
-## ______________________________________________________________________ N=2
-	if N>1:
-		F_Hfilter2 = v*u_3tilde*dx + delta*delta*dot(grad(v), grad(u_3tilde))*dx - v*u_2tilde*dx
-
-		a_Hfilter2 = lhs(F_Hfilter2)
-		L_Hfilter2 = rhs(F_Hfilter2)
-
-		A_Hfilter2 = assemble(a_Hfilter2)
-		bc.apply(A_Hfilter2)
-
-		b_Hfilter2 = assemble(L_Hfilter2)
-		bc.apply(b_Hfilter2)
-
-		solver2 = LUSolver(A_Hfilter2)
-		u_3tilde = Function(Q)
-		solver2.solve(u_3tilde.vector(), b_Hfilter2)
-		DF = Expression('a+b-2*c+d',degree=2,a=DF,b=u_1tilde,c=u_2tilde,d=u_3tilde)
-		out_file_u3tilde << u_3tilde
-		ind3 = a(u_tilde, DF)
-		out_file_ind3 << ind3
-		if N==2:
-			ind = ind3
-
-## ______________________________________________________________________ N=3
-		if N>2:
-			F_Hfilter3 = v*u_4tilde*dx + delta*delta*dot(grad(v), grad(u_4tilde))*dx - v*u_3tilde*dx
-
-			a_Hfilter3 = lhs(F_Hfilter3)
-			L_Hfilter3 = rhs(F_Hfilter3)
-
-			A_Hfilter3 = assemble(a_Hfilter3)
-			bc.apply(A_Hfilter3)
-
-			b_Hfilter3 = assemble(L_Hfilter3)
-			bc.apply(b_Hfilter3)
-
-			solver3 = LUSolver(A_Hfilter3)
-			u_4tilde = Function(Q)
-			solver3.solve(u_4tilde.vector(), b_Hfilter3)
-			DF = Expression('a+b-3*c+3*d-e',degree=2,a=DF,b=u_1tilde,c=u_2tilde,d=u_3tilde,e=u_4tilde)
-			out_file_u4tilde << u_4tilde
-			ind4 = a(u_tilde, DF)
-			out_file_ind4 << ind4
-			if N == 3:
-				ind = ind4
+	out_file_u1tilde = File(folder+"/u1_tilde.pvd")
+	out_file_u2tilde = File(folder+"/u2_tilde.pvd")
+	out_file_u3tilde = File(folder+"/u3_tilde.pvd")
+	out_file_u4tilde = File(folder+"/u4_tilde.pvd")
 
 
 
 
+		## ______________________________________________________________________ N=0
 
-# Apply the filter
 
-u_bar = TrialFunction(Q)
-if SU == 1:
-	F_filter = v*u_bar*dx + delta*delta*dot(dot(velocity,grad(v)), ind*dot(velocity, grad(u_bar)))*dx - v*u*dx 
-	out_file_ubar = File(folder+before+after+"u_bar_SU.pvd")
-else:
-	F_filter = v*u_bar*dx + delta*delta*dot(grad(v), ind*grad(u_bar))*dx - v*u*dx 
-	out_file_ubar = File(folder+before+after+"u_bar.pvd")
 
-a_filter = lhs(F_filter)
-L_filter = rhs(F_filter)
+	def a(u_, DF):
+		# Compute the indicator function
+		indicator = Expression('sqrt((a-b)*(a-b))', degree = 2, a = u_, b = DF)
+		indicator = interpolate(indicator, Q)
+		max_ind = np.amax(indicator.vector().array())
 
-A_filter = assemble(a_filter)
-bc.apply(A_filter)
+		# Normalize indicator such that it's between [0,1].
+		if max_ind < 1:
+		   max_ind = 1.0
 
-b_filter = assemble(L_filter)
-bc.apply(b_filter)
+		indicator = Expression('a/b', degree = 2, a = indicator, b = max_ind)
+		indicator = interpolate(indicator, Q)
+		return indicator
 
-solver = LUSolver(A_filter)
-u_bar = Function(Q)
-solver.solve(u_bar.vector(), b_filter)
-'''
-# Save Output Files
-if N==0:
-	nofilter_Linf_err = np.abs(u_SUPG.vector().array() - u.vector().array()).max()
-	filtered_Linf_err = np.abs(u_SUPG.vector().array() - u_bar.vector().array()).max()
-	nofilter_L2_err = errornorm(u_SUPG, u, 'L2')
-	filtered_L2_err = errornorm(u_SUPG,u_bar,'L2')
-	firstcol = "$h=1/"+str(nx)+"$, "+"$\delta = "+str(scalename)+"h $"
-	outputf = firstcol+"& "+str(round(nofilter_Linf_err,4))+" & "+str(round(filtered_Linf_err,4))
-	outputg = firstcol+"& "+str(round(nofilter_L2_err,4))+" & "+str(round(filtered_L2_err,4))
-if N >0:				
-	filtered_Linf_err = np.abs(u_SUPG.vector().array() - u_bar.vector().array()).max()
-	filtered_L2_err = errornorm(u_SUPG,u_bar,'L2')	
-	outputf = " & "+str(round(filtered_Linf_err,4))
-	outputg = " & "+str(round(filtered_L2_err,4))
-	if N==3:
-		outputf = outputf+"  \\\\ \n \\hline \n"	
-		outputg = outputg+"  \\\\ \n \\hline \n"
-f=open("P"+str(P)+"_infnorm.txt","a+")
-g=open("P"+str(P)+"_2norm.txt","a+")			
-f.write(outputf)
-g.write(outputg)
-'''
 
-out_file_ubar << u_bar
+
+	F_Hfilter0 = v*u_1tilde*dx + delta*delta*dot(grad(v), grad(u_1tilde))*dx - v*u*dx
+
+	a_Hfilter0 = lhs(F_Hfilter0)
+	L_Hfilter0 = rhs(F_Hfilter0)
+
+	A_Hfilter0 = assemble(a_Hfilter0)
+	bc.apply(A_Hfilter0)
+
+	b_Hfilter0 = assemble(L_Hfilter0)
+	bc.apply(b_Hfilter0)
+
+	solver0 = LUSolver(A_Hfilter0)
+	u_1tilde = Function(Q)
+	solver0.solve(u_1tilde.vector(), b_Hfilter0)
+	DF = u_1tilde
+	out_file_u1tilde << u_1tilde
+
+	ind1 = a(u_tilde, DF)
+	out_file_ind1 << ind1
+	if N == 0:
+		ind = ind1
+
+	## ______________________________________________________________________ N=1
+	if N>0:
+		F_Hfilter1 = v*u_2tilde*dx + delta*delta*dot(grad(v), grad(u_2tilde))*dx - v*u_1tilde*dx
+
+		a_Hfilter1 = lhs(F_Hfilter1)
+		L_Hfilter1 = rhs(F_Hfilter1)
+
+		A_Hfilter1 = assemble(a_Hfilter1)
+		bc.apply(A_Hfilter1)
+
+		b_Hfilter1 = assemble(L_Hfilter1)
+		bc.apply(b_Hfilter1)
+
+		solver1 = LUSolver(A_Hfilter1)
+		u_2tilde = Function(Q)
+		solver1.solve(u_2tilde.vector(), b_Hfilter1)
+		DF = Expression('a+b-c',degree=2,a=DF,b=u_1tilde,c=u_2tilde)
+		out_file_u2tilde << u_2tilde
+		ind2 = a(u_tilde, DF)
+		out_file_ind2 << ind2
+		if N==1:
+			ind = ind2
+
+
+	## ______________________________________________________________________ N=2
+		if N>1:
+			F_Hfilter2 = v*u_3tilde*dx + delta*delta*dot(grad(v), grad(u_3tilde))*dx - v*u_2tilde*dx
+
+			a_Hfilter2 = lhs(F_Hfilter2)
+			L_Hfilter2 = rhs(F_Hfilter2)
+
+			A_Hfilter2 = assemble(a_Hfilter2)
+			bc.apply(A_Hfilter2)
+
+			b_Hfilter2 = assemble(L_Hfilter2)
+			bc.apply(b_Hfilter2)
+
+			solver2 = LUSolver(A_Hfilter2)
+			u_3tilde = Function(Q)
+			solver2.solve(u_3tilde.vector(), b_Hfilter2)
+			DF = Expression('a+b-2*c+d',degree=2,a=DF,b=u_1tilde,c=u_2tilde,d=u_3tilde)
+			out_file_u3tilde << u_3tilde
+			ind3 = a(u_tilde, DF)
+			out_file_ind3 << ind3
+			if N==2:
+				ind = ind3
+
+	## ______________________________________________________________________ N=3
+			if N>2:
+				F_Hfilter3 = v*u_4tilde*dx + delta*delta*dot(grad(v), grad(u_4tilde))*dx - v*u_3tilde*dx
+
+				a_Hfilter3 = lhs(F_Hfilter3)
+				L_Hfilter3 = rhs(F_Hfilter3)
+
+				A_Hfilter3 = assemble(a_Hfilter3)
+				bc.apply(A_Hfilter3)
+
+				b_Hfilter3 = assemble(L_Hfilter3)
+				bc.apply(b_Hfilter3)
+
+				solver3 = LUSolver(A_Hfilter3)
+				u_4tilde = Function(Q)
+				solver3.solve(u_4tilde.vector(), b_Hfilter3)
+				DF = Expression('a+b-3*c+3*d-e',degree=2,a=DF,b=u_1tilde,c=u_2tilde,d=u_3tilde,e=u_4tilde)
+				out_file_u4tilde << u_4tilde
+				ind4 = a(u_tilde, DF)
+				out_file_ind4 << ind4
+				if N == 3:
+					ind = ind4
+
+
+
+
+
+	# Apply the filter
+
+	u_bar = TrialFunction(Q)
+	if SU == 1:
+		F_filter = v*u_bar*dx + delta*delta*dot(dot(velocity,grad(v)), ind*dot(velocity, grad(u_bar)))*dx - v*u*dx 
+		out_file_ubar = File(folder+before+after+"u_bar_SU.pvd")
+	else:
+		F_filter = v*u_bar*dx + delta*delta*dot(grad(v), ind*grad(u_bar))*dx - v*u*dx 
+		out_file_ubar = File(folder+before+after+"u_bar.pvd")
+
+	a_filter = lhs(F_filter)
+	L_filter = rhs(F_filter)
+
+	A_filter = assemble(a_filter)
+	bc.apply(A_filter)
+
+	b_filter = assemble(L_filter)
+	bc.apply(b_filter)
+
+	solver = LUSolver(A_filter)
+	u_bar = Function(Q)
+	solver.solve(u_bar.vector(), b_filter)
+	'''
+	# Save Output Files
+	if N==0:
+		nofilter_Linf_err = np.abs(u_SUPG.vector().array() - u.vector().array()).max()
+		filtered_Linf_err = np.abs(u_SUPG.vector().array() - u_bar.vector().array()).max()
+		nofilter_L2_err = errornorm(u_SUPG, u, 'L2')
+		filtered_L2_err = errornorm(u_SUPG,u_bar,'L2')
+		firstcol = "$h=1/"+str(nx)+"$, "+"$\delta = "+str(scalename)+"h $"
+		outputf = firstcol+"& "+str(round(nofilter_Linf_err,4))+" & "+str(round(filtered_Linf_err,4))
+		outputg = firstcol+"& "+str(round(nofilter_L2_err,4))+" & "+str(round(filtered_L2_err,4))
+	if N >0:				
+		filtered_Linf_err = np.abs(u_SUPG.vector().array() - u_bar.vector().array()).max()
+		filtered_L2_err = errornorm(u_SUPG,u_bar,'L2')	
+		outputf = " & "+str(round(filtered_Linf_err,4))
+		outputg = " & "+str(round(filtered_L2_err,4))
+		if N==3:
+			outputf = outputf+"  \\\\ \n \\hline \n"	
+			outputg = outputg+"  \\\\ \n \\hline \n"
+	f=open("P"+str(P)+"_infnorm.txt","a+")
+	g=open("P"+str(P)+"_2norm.txt","a+")			
+	f.write(outputf)
+	g.write(outputg)
+	'''
+
+	out_file_ubar << u_bar
