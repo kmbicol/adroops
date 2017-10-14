8985ee8fdcf490d133222af9f3fbabbff5c37b45
Kayla Bicol <kmbicol@math.uh.edu>
2017-10-14 05:03:17 -0500

    cleaned up code to match with steady_simp

--- 2D_adr/adr_time_dep_mod.py
+++ 2D_adr/adr_time_dep_mod.py
@@ -1,61 +1,85 @@
-from __future__ import print_function
 from dolfin import *
 from fenics import *
-from copy import deepcopy
 import numpy as np
 import sympy as sym
+import math as m
 
-code = 'Iliescu'
-EFR = 1
+#test = input('1 f=t, 2 Iliescu, 3 Manufactured ')
+test = 1
+EFR = 0 #EFR = input('EFR: (1) Yes (0) No ') #EFR = 0
+nx = input('h=1/nx, set nx as ') #nx = 100
+delta = 1.0/20.0  # filtering radius
 
-# Parameters
-if code == 'Test':
+P = 1
 
-	# My test problem
+# Create mesh
+mesh = UnitSquareMesh(nx,nx)
+h = CellSize(mesh)
 
-	T = 1.0 # final time
-	dt = 0.01 # time step size
-	tt = 0.01
-	num_steps = int(round(T / tt, 0))           
+# Define function spaces
+Q = FunctionSpace(mesh, "CG", P)
+t = 0
 
-	sigma = 1.0       # reaction coefficient
-	mu = 0.001        # diffision coefficient
-	delta = 1.0/50.0  # filtering radius
-
-	velocity = as_vector([1.0, 1.0]) # convection velocity
-
-
-if code == 'Iliescu':
-
-    # Iliescu Ex 4.1
-
-    T = 2.0 #2.0 # final time
+# Simulation Parameters
+if test == 1:
+    # My test problem
+    T = 1.0 # final time
     dt = 0.01 # time step size
     tt = 0.01
     num_steps = int(round(T / tt, 0))           
 
+    sigma = 0.01
+    mu = 0.001
+    velocity = as_vector([1.0, 1.0]) # convection velocity
+    u_D = Constant(0.0)
+    u_exact = Constant(0.0)
+    adr_f = Expression('t', degree = 1, t=t)
+
+    folder = 'results_time_dep_simp'
+    folder +="/P"+str(P)+"h1_"+str(nx)+"/"
+
+elif test == 2:
+    # Iliescu Ex 4.1
+    T = 2.0 #2.0 # final time
+    dt = 0.001 # time step size
+    tt = 0.001
+    num_steps = int(round(T / tt, 0))           
+
     sigma = 1.0       # reaction coefficient
-    mu = 0.000001        # diffusion coefficient
-    delta = 1.0/20.0  # filtering radius
+    mu = 10**(-6)        # diffusion coefficient
 
     velocity = as_vector([2.0, 3.0]) # convection velocity
     a = velocity[0]
     b = velocity[1]
+    folder = 'results_time_dep_ilie'
+    folder +="/P"+str(P)+"h1_"+str(nx)+"/"
+
+elif test == 3:
+    # My test problem
+    T = 1.0 # final time
+    dt = 0.01 # time step size
+    tt = 0.01
+    num_steps = int(round(T / tt, 0))           
+
+    sigma = 0.01
+    mu = 0.001
+    velocity = as_vector([1.0, 1.0]) # convection velocity
+    u_exact = Expression('t+x[0]+x[1]', degree = 1, sigma = sigma, mu = mu)
+    adr_f = Expression('3+sigma*(t+x[0]+x[1])', degree = 1, sigma = sigma, mu=mu, t = t)
+    u_D = Expression(u_exact.cppcode, degree = 1, sigma = sigma, mu=mu)
+
+    folder = 'results_time_dep_simp'
+    folder +="/P"+str(P)+"h1_"+str(nx)+"/"
+
+else:
+    test = input('Choose 1, 2, or 3...')
 ###########################################################
-
-# Create mesh
-mesh = UnitSquareMesh(20,20)
-h = CellSize(mesh)
-
-# Define function spaces
-Q = FunctionSpace(mesh, "CG", 1)
 
 # Define boundaries
 def boundary(x, on_boundary):
     return on_boundary
 
 # Define inflow profile ?
-
 
 
 # Define trial and test functions (not computed yet)
@@ -67,26 +91,20 @@ u_n, u_ = Function(Q), Function(Q)
 u_n_SUPG, u_SUPG_ = Function(Q), Function(Q)
 u_n_GLS, u_GLS_ = Function(Q), Function(Q)
 u_n_DW, u_DW_ = Function(Q), Function(Q)
+u_n_VMS, u_VMS_ = Function(Q), Function(Q)
 u_tilde = Function(Q)
 u_bar = Function(Q)
 
 # Define expressions used in variational forms
-t = 0
-
-if code == 'Test':
-    ue = Expression('t+x[0]+x[1]', degree = 1, sigma = sigma, mu = mu)
-    adr_f = Expression('3+sigma*(t+x[0]+x[1])', degree = 1, sigma = sigma, mu=mu, t = t)
-    uee = sym.printing.ccode(ue)
-if code == 'Iliescu':
-    ue = Expression('-16*x[0]*x[1]*(x[0] - 1)*(x[1] - 1)*(0.318309886183791*atan(2000.0*pow(x[0] - 0.5, 2) + 2000.0*pow(x[1] - 0.5, 2) - 125.0) - 0.5)*sin(3.14159265358979*t)', degree = 1, t = t)
+if test == 3:
+    u_D = Constant(0.0)
+    u_exact = Expression('-16*x[0]*x[1]*(x[0] - 1)*(x[1] - 1)*(0.318309886183791*atan(2000.0*pow(x[0] - 0.5, 2) + 2000.0*pow(x[1] - 0.5, 2) - 125.0) - 0.5)*sin(3.14159265358979*t)', degree = 1, t = t)
     adr_f = Expression('(-5.09295817894065e-6*x[0]*x[1]*(x[0] - 1)*(x[1] - 1)*((4000.0*x[0] - 2000.0)*(8000.0*x[0] - 4000.0) + (4000.0*x[1] - 2000.0)*(8000.0*x[1] - 4000.0))*(2000.0*pow(x[0] - 0.5, 2) + 2000.0*pow(x[1] - 0.5, 2) - 125.0) + pow(pow(2000.0*pow(x[0] - 0.5, 2) + 2000.0*pow(x[1] - 0.5, 2) - 125.0, 2) + 1, 2)*(0.318309886183791*atan(2000.0*pow(x[0] - 0.5, 2) + 2000.0*pow(x[1] - 0.5, 2) - 125.0) - 0.5)*(-16.0*x[0]*x[1]*(x[0] - 1)*(x[1] - 1) - 48.0*x[0]*x[1]*(x[0] - 1) - 32.0*x[0]*x[1]*(x[1] - 1) - 48.0*x[0]*(x[0] - 1)*(x[1] - 1) + 3.2e-5*x[0]*(x[0] - 1) - 32.0*x[1]*(x[0] - 1)*(x[1] - 1) + 3.2e-5*x[1]*(x[1] - 1)) + (pow(2000.0*pow(x[0] - 0.5, 2) + 2000.0*pow(x[1] - 0.5, 2) - 125.0, 2) + 1)*(-10.1859163578813*x[0]*x[1]*(x[0] - 1)*(4000.0*x[0] - 2000.0)*(x[1] - 1) - 15.278874536822*x[0]*x[1]*(x[0] - 1)*(x[1] - 1)*(4000.0*x[1] - 2000.0) + 0.0407436654315252*x[0]*x[1]*(x[0] - 1)*(x[1] - 1) + 1.01859163578813e-5*x[0]*x[1]*(x[0] - 1)*(4000.0*x[1] - 2000.0) + 1.01859163578813e-5*x[0]*x[1]*(4000.0*x[0] - 2000.0)*(x[1] - 1) + 1.01859163578813e-5*x[0]*(x[0] - 1)*(x[1] - 1)*(4000.0*x[1] - 2000.0) + 1.01859163578813e-5*x[1]*(x[0] - 1)*(4000.0*x[0] - 2000.0)*(x[1] - 1)))*sin(3.14159265358979*t)/pow(pow(2000.0*pow(x[0] - 0.5, 2) + 2000.0*pow(x[1] - 0.5, 2) - 125.0, 2) + 1, 2)', degree = 1, t=t)
-out_file_ue = File('results_time_dep/uexact.pvd')
 
 f_n = Expression(adr_f.cppcode, degree = 1, t = t) 
 f = Expression(adr_f.cppcode, degree = 1, t = t+tt)
 
 # Define boundary condition
-u_D = Expression(ue.cppcode, degree = 1, t = t)
 bc = DirichletBC(Q, u_D, boundary)
 
 # Define for time stepping
@@ -120,20 +138,32 @@ if EFR == 0:
     r = - mu*div(grad(u)) + dot(velocity, grad(u)) + sigma*u - f # Lu - f
     vnorm = sqrt(dot(velocity, velocity))
 
+    # tau Stabilization Parameter
+
+    tau = h/(2.0*vnorm)
+
     # SUPG stabilisation terms
-    F_SUPG = F1 + (h/(2.0*vnorm))*dot(velocity, grad(v))*r*dx
+#    F_SUPG = F1 + tau*dot(velocity, grad(v))*r*dx
+    F_SUPG = F1 + tau*(0.5*div(velocity*v)+0.5*dot(velocity, grad(v)))*r*dx
     a_SUPG = lhs(F_SUPG)
     L_SUPG = rhs(F_SUPG)
 
     # GLS stabilization terms
-    F_GLS = F1 + (h/(2.0*vnorm))*(- mu*div(grad(v)) + dot(velocity, grad(v)) + sigma*v)*r*dx #LvRu
+    F_GLS = F1 + tau*(- mu*div(grad(v)) + dot(velocity, grad(v)) + sigma*v)*r*dx #LvRu
     a_GLS = lhs(F_GLS)
     L_GLS = rhs(F_GLS)
 
     # DW stabilization terms
-    F_DW = F1 - (h/(2.0*vnorm))*(- mu*div(grad(v)) - dot(velocity, grad(v)) + sigma*v)*r*dx #LvRu
+    F_DW = F1 - tau*(- mu*div(grad(v)) - dot(velocity, grad(v)) + sigma*v)*r*dx #LvRu
     a_DW = lhs(F_DW)
     L_DW = rhs(F_DW)
+
+    # DW stabilization terms
+    hh = 1.0/nx
+    ttau = m.pow((4.0*mu/(hh*hh) + 2.0*vnorm/hh + sigma),-1)
+    F_VMS = F1 - (ttau)*(- mu*div(grad(v)) - dot(velocity, grad(v)) + sigma*v)*r*dx #LvRu
+    a_VMS = lhs(F_VMS)
+    L_VMS = rhs(F_VMS)
 
     # Assemble matrix
     A1 = assemble(a1)
@@ -148,12 +178,17 @@ if EFR == 0:
     A_DW = assemble(a_DW)
     bc.apply(A_DW)
 
+    A_VMS = assemble(a_VMS)
+    bc.apply(A_VMS)
+
     # Create VTK files for visualization output
-    folder = "results_time_dep/"
-    out_file_nofilter = File(folder+"u_nofilter.pvd")              # no filter solution
-    out_file_supg = File(folder+"u_SUPG.pvd")  # u tilde
-    out_file_gls = File(folder+"u_GLS.pvd")          # indicator function
-    out_file_dw = File(folder+"u_DW.pvd")      # filtered solution
+    out_file_nofilter = File(folder+"u_nofilter_h1_"+str(nx)+"t.pvd")              # no filter solution
+    out_file_supg = File(folder+"u_SUPG_h1_"+str(nx)+"t.pvd")  # u tilde
+    out_file_gls = File(folder+"u_GLS_h1_"+str(nx)+"t.pvd")          # indicator function
+    out_file_ue = File(folder+"u_exact_h1_"+str(nx)+"t.pvd")
+    out_file_fe = File(folder+"f_exact_h1_"+str(nx)+"t.pvd")
+    out_file_dw = File(folder+"u_DW_h1_"+str(nx)+"t.pvd")      
+    out_file_vms = File(folder+"u_VMS_h1_"+str(nx)+"t.pvd")      # filtered solution
 
     for n in range(num_steps):
         # Update current time
@@ -176,28 +211,37 @@ if EFR == 0:
         bc.apply(b_DW)
         solve(A_DW, u_DW_.vector(), b_DW)
 
+        b_VMS = assemble(L_VMS)
+        bc.apply(b_VMS)
+        solve(A_VMS, u_VMS_.vector(), b_VMS)
+
         # Save solution to file (VTK)
         out_file_nofilter << (u_, float(t))
         out_file_supg << (u_SUPG_, float(t))
         out_file_gls << (u_GLS_, float(t))
         out_file_dw << (u_DW_, float(t))
+        out_file_vms << (u_VMS_, float(t))
 
         # Update previous solution and source term
         u_n.assign(u_)
         u_n_SUPG.assign(u_SUPG_)
         u_n_GLS.assign(u_GLS_)
         u_n_DW.assign(u_DW_)
+        u_n_VMS.assign(u_VMS_)
 
         f_n = Expression(f.cppcode, degree = 1, sigma = sigma, t = t)
+        fee = interpolate(f_n, Q)
+        fee.rename('f','f')
+        out_file_fe << (fee, float(t+tt))
         f.t += tt
         f_n.t += tt
 
         # Exact solution in time
-        ue = Expression('-16*x[0]*x[1]*(x[0] - 1)*(x[1] - 1)*(0.318309886183791*atan(2000.0*pow(x[0] - 0.5, 2) + 2000.0*pow(x[1] - 0.5, 2) - 125.0) - 0.5)*sin(3.14159265358979*t)', degree = 1, t = t)
-        uee = interpolate(ue, Q)
-        uee.rename('u','u')
-        out_file_ue << (uee, float(t+tt))
-        ue.t += tt
+        #ue = Expression(u_exact.cppcode, degree = 1, t = t)
+        #uee = interpolate(ue, Q)
+        #uee.rename('u','u')
+        #out_file_ue << (uee, float(t+tt))
+        #u_exact.t += tt
 
         # Update progress bar
         progress.update(t / T)
@@ -209,7 +253,7 @@ if EFR == 0:
 if EFR == 1:
 
     # Define indicator function to evaluate current time step
-    def a(u_tilde, u_):
+    def a(u_tilde, u_, t):
         indicator = Expression('sqrt((a-b)*(a-b))', degree = 2, a = u_, b = u_tilde)
         indicator = interpolate(indicator, Q)
         max_ind = np.amax(indicator.vector().array())
@@ -220,6 +264,8 @@ if EFR == 1:
 
         indicator = Expression('a/b', degree = 2, a = indicator, b = max_ind)
         indicator = interpolate(indicator, Q) 
+        indicator.rename('a','a')
+        out_file_ind << (indicator, float(t))
         return indicator
 
     t = 0
@@ -246,8 +292,7 @@ if EFR == 1:
 
 
     # Create VTK files for visualization output
-    folder = "results_time_dep_iliescu/"
-    out_file = File(folder+"u.pvd")              # no filter solution
+    out_file = File(folder+"u_nofilter.pvd")              # no filter solution
     out_file_utilde = File(folder+"utilde.pvd")  # u tilde
     out_file_ind = File(folder+"a.pvd")          # indicator function
     out_file_ubar = File(folder+"ubar.pvd")      # filtered solution
@@ -269,7 +314,7 @@ if EFR == 1:
         solve(A2, u_tilde.vector(), b2)
 
         # Step 2b
-        ind = a(u_tilde, u_)
+        ind = a(u_tilde, u_, t)
         A3 = assemble(a3(ind))
         bc.apply(A3)
         b3 = assemble(L3)
@@ -279,7 +324,6 @@ if EFR == 1:
         # Save solution to file (VTK)
         out_file << (u_, float(t))
         out_file_utilde << (u_tilde, float(t))
-        out_file_ind << (ind, float(t))
         out_file_ubar << (u_bar, float(t))
 
         # Update previous solution and source term
