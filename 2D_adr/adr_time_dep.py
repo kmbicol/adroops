45c9f8ab7256c4148acb55d94a9906d5a7b81c6f
Kayla Bicol <kmbicol@math.uh.edu>
2017-11-03 00:30:31 -0500

    this version has non-uniform velocity implemented

--- 2D_adr/adr_time_dep.py
+++ 2D_adr/adr_time_dep.py
@@ -2,26 +2,22 @@ from dolfin import *
 import math as m
 import numpy as np
 
-#print("\n (1) SUPG \n (2) GLS \n (3) DW \n (4) VMS \n (5) Galerkin and Exact Solution \n (default) Galerkin")
-#method = input("Choose a stabilization method: ")
+print("\n (1) SUPG \n (2) GLS \n (3) DW \n (4) VMS \n (5) Galerkin and Exact Solution \n (default) Galerkin")
+method = input("Choose a stabilization method: ")
 #nx = input("h = 1/nx, let nx = ")
 
-method = 1
+#method = 1
 
 # Simulation Parameters
 nx = 300
 T = 0.520 #2.0
 dt = 0.001
 t = dt
-sigma = 1.0
-mu = 10**(-6)
 R = 2
 P = 2
 saveTimesteps = 5 # save every __ time steps
-folder = "results_full_ilie_BE/h"+str(nx)
+folder = "results_nonunifb_BE/h"+str(nx)
 
-# Create velocity Function from file
-velocity = as_vector([2.0, 3.0])
 
 # Load mesh and subdomains
 mesh = UnitSquareMesh(nx,nx)
@@ -29,10 +25,6 @@ h = CellSize(mesh)
 
 # Create FunctionSpaces
 Q = FunctionSpace(mesh, "CG", P)
-
-
-# Boundary values
-u_D = Constant(0.0)
 
 # Initialise source function and previous solution function
 
@@ -43,11 +35,40 @@ u_n = Function(Q)
 u, v = TrialFunction(Q), TestFunction(Q)
 u_ = Function(Q)
 
+'''
+# Iliescu Ex 4.1
+sigma = 1.0
+mu = 10**(-6)
+velocity = as_vector([2.0, 3.0])
+
+u_D = Constant(0.0) # Boundary values
+
 u_exact = Expression('-16*x[0]*x[1]*(x[0] - 1)*(x[1] - 1)*(0.318309886183791*atan(2000.0*pow(x[0] - 0.5, 2) + 2000.0*pow(x[1] - 0.5, 2) - 125.0) - 0.5)*sin(3.14159265358979*t)', degree = R, t = t)
 adr_f = Expression('(-5.09295817894065e-6*x[0]*x[1]*(x[0] - 1)*(x[1] - 1)*((4000.0*x[0] - 2000.0)*(8000.0*x[0] - 4000.0) + (4000.0*x[1] - 2000.0)*(8000.0*x[1] - 4000.0))*(2000.0*pow(x[0] - 0.5, 2) + 2000.0*pow(x[1] - 0.5, 2) - 125.0)*sin(3.14159265358979*t) + pow(pow(2000.0*pow(x[0] - 0.5, 2) + 2000.0*pow(x[1] - 0.5, 2) - 125.0, 2) + 1, 2)*(0.318309886183791*atan(2000.0*pow(x[0] - 0.5, 2) + 2000.0*pow(x[1] - 0.5, 2) - 125.0) - 0.5)*(-16.0*x[0]*x[1]*(x[0] - 1)*(x[1] - 1)*sin(3.14159265358979*t) - 50.2654824574367*x[0]*x[1]*(x[0] - 1)*(x[1] - 1)*cos(3.14159265358979*t) - 48.0*x[0]*x[1]*(x[0] - 1)*sin(3.14159265358979*t) - 32.0*x[0]*x[1]*(x[1] - 1)*sin(3.14159265358979*t) - 48.0*x[0]*(x[0] - 1)*(x[1] - 1)*sin(3.14159265358979*t) + 3.2e-5*x[0]*(x[0] - 1)*sin(3.14159265358979*t) - 32.0*x[1]*(x[0] - 1)*(x[1] - 1)*sin(3.14159265358979*t) + 3.2e-5*x[1]*(x[1] - 1)*sin(3.14159265358979*t)) + (pow(2000.0*pow(x[0] - 0.5, 2) + 2000.0*pow(x[1] - 0.5, 2) - 125.0, 2) + 1)*(-10.1859163578813*x[0]*x[1]*(x[0] - 1)*(4000.0*x[0] - 2000.0)*(x[1] - 1) - 15.278874536822*x[0]*x[1]*(x[0] - 1)*(x[1] - 1)*(4000.0*x[1] - 2000.0) + 0.0407436654315252*x[0]*x[1]*(x[0] - 1)*(x[1] - 1) + 1.01859163578813e-5*x[0]*x[1]*(x[0] - 1)*(4000.0*x[1] - 2000.0) + 1.01859163578813e-5*x[0]*x[1]*(4000.0*x[0] - 2000.0)*(x[1] - 1) + 1.01859163578813e-5*x[0]*(x[0] - 1)*(x[1] - 1)*(4000.0*x[1] - 2000.0) + 1.01859163578813e-5*x[1]*(x[0] - 1)*(4000.0*x[0] - 2000.0)*(x[1] - 1))*sin(3.14159265358979*t))/pow(pow(2000.0*pow(x[0] - 0.5, 2) + 2000.0*pow(x[1] - 0.5, 2) - 125.0, 2) + 1, 2)', degree = R, t = t)
+'''
+
+# Non-uniform convective velocity
+sigma = 1.0       # reaction coefficient
+mu = 0.001        # diffision coefficient
+velocity = Expression(('cos(t)', 'sin(t)') , degree=2, t=t)
+u_exact = Expression(' pow(x[0], 2) + pow(x[1], 2) ', degree=R, t=t)
+u_D = Expression(u_exact.cppcode, degree=R, t=0)
+#u_D = interpolate(u_D,Q)
+
+adr_f = Expression(' 1.0*pow(x[0], 2) + 2*x[0]*cos(t) + 1.0*pow(x[1], 2) + 2*x[1]*sin(t) - 0.004 ', degree=R, t=t)
+
+
+
+
 
 f = Expression(adr_f.cppcode, degree = R, t = t)
 f0 = Expression(adr_f.cppcode, degree = R, t = 0)
+
+
+
+
+
+
 '''
 # Mid-point solution
 u_mid = 0.5*(u_n + u)
@@ -167,6 +188,7 @@ if method != 6:
         u_n = u
         f0.t += dt
         f.t += dt
+        u_D.t += dt
 
         # Save the solution to file
         # Save solution to file (VTK)
