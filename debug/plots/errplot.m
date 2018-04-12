4b420defbfd94bafb7f71017e2299b796fa5a9b4
Kayla Bicol <kmbicol@math.uh.edu>
2018-04-12 16:54:36 -0500

    adding more debug stuff

--- debug/plots/errplot.m
+++ debug/plots/errplot.m
@@ -6,8 +6,9 @@ function [ ] = errplot( L2, H1, method, color)
     H1size = size(H1,2);
     w=1.5;
     r = 10^4; b = 10^-2;
-    h2 = [r r/(2^2) r/(4^2) r/(8^2) r/(16^2) r/(32^2) r/(64^2)];
-    h3 = [b b/(2^3) b/(4^3) b/(8^3) b/(16^3) b/(32^3) b/(64^3)];
+    g = 2; d = 3;
+    h2 = [r r/(2^g) r/(4^g) r/(8^g) r/(16^g) r/(32^g) r/(64^g)];
+    h3 = [b b/(2^d) b/(4^d) b/(8^d) b/(16^d) b/(32^d) b/(64^d)];
     fig = figure();
     level = 0:5;
     semilogy(level,L2, [color 'o-.'],'LineWidth',w)
